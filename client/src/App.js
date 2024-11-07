f0e75c1551c0030060b38e58a72334836573fba3
Jose A Lopez Jr <flopez0294@gmail.com>
2024-11-07 09:51:42 -0800

    basic get fetch is working

---
+++ client/src/App.js
@@ -0,0 +1,25 @@
+import React, {useEffect, useState} from 'react'
+import './App.css';
+
+function App() {
+  const [yo,setData] = useState({});
+
+  useEffect( () => {
+    fetch('/member')
+      .then((res) => {
+        return res.json();
+      })
+      .then((data) => {
+        // console.log(data);
+        // setData(data);
+      });
+  }, [])
+
+  return (
+    <div className="App">
+     
+    </div>
+  );
+}
+
+export default App;
